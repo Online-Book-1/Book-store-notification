76459c85882af49422aa25553d80c67dff50b88e
Hritik <hritikchoukikar2000@gmail.com>
2026-04-21 15:43:35 +0530

    updated code

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ Base=declarative_base()
 class Notification(Base):
     __tablename__="user_notification"
     id=Column(Integer,autoincrement=True,primary_key=True)
-    user_id=Column(Integer,nullable=False),
+    user_id=Column(Integer,nullable=False)
     notification_data=Column(Text,nullable=False)
     is_read=Column(Boolean,default=False)
 
