8d85104ac7fa18beb3bb53aaa8ac1b9d117a0933
Hritik <hritikchoukikar2000@gmail.com>
2026-04-21 15:48:26 +0530

    updated code

--- db_sender.py
+++ db_sender.py
@@ -9,15 +9,15 @@ from models import Notification
 
 
 
-db_consumer=KafkaConsumer('notify.notifications',group_id="notify.sms.group",
-                             bootstrap_servers=[settings.KAFKA_BOOTSTRAP_SERVERS],
-                             value_deserializer=lambda x: json.loads(x.decode("utf-8")))
-
-
 
 def start():
     print("db service Started... Listening for events to route via Email.")
     
+
+    db_consumer=KafkaConsumer('notify.notifications',group_id="notify.sms.group",
+                             bootstrap_servers=[settings.KAFKA_BOOTSTRAP_SERVERS],
+                             value_deserializer=lambda x: json.loads(x.decode("utf-8")))
+
 
     for msg in db_consumer:
         data = msg.value
--- sms_sender.py
+++ sms_sender.py
@@ -5,17 +5,18 @@ from config import settings
 
 
 
-email_consumer=KafkaConsumer('notify.sms',group_id="notify.sms.group",
-                             bootstrap_servers=[settings.KAFKA_BOOTSTRAP_SERVERS],
-                             value_deserializer=lambda x: json.loads(x.decode("utf-8")))
-
-
 def send_sms(to,body):
     print(f"SMS:sent")                      
 
 
 def start():
     print("SMS Started... Listening for events to route via sms.")
+    
+
+    email_consumer=KafkaConsumer('notify.sms',group_id="notify.sms.group",
+                             bootstrap_servers=[settings.KAFKA_BOOTSTRAP_SERVERS],
+                             value_deserializer=lambda x: json.loads(x.decode("utf-8")))
+
     
     for msg in email_consumer:
         message=msg.value
